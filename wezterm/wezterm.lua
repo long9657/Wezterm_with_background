7e283ab02608a72f2891d2b1041c0607bdf74901
long9657 <longlehien9657@gmail.com>
2025-04-05 17:15:53 +0700

    changes that make me satisfied

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,12 +7,18 @@ local config = wezterm.config_builder()
 local brightness = 0.03
 
 -- Check if the OS is Windows or Unix-based
-local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
-local bg_image = home_dir .. "/.config/wezterm/bg/f.jpg"
+local background_folder = wezterm.config_dir .. "/bg"
+local bg_image = wezterm.config_dir .. "/bg/j.jpg"
+local function pick_random_background(folder)
+	local file_name = string.char(math.random(string.byte("d"), string.byte("j")))
+	return folder .. "/" .. file_name .. ".jpg"
+end
 
 -- local gpus = wezterm.gui.enumerate_gpus()
 -- config.webgpu_preferred_adapter = gpus[1]
 -- config.front_end = "WebGpu"
+config.front_end = "OpenGL"
+config.max_fps = 144
 
 config.window_padding = {
 	left = "0",
@@ -20,33 +26,27 @@ config.window_padding = {
 	top = "0",
 	bottom = "0",
 }
+
 config.term = "xterm-256color" -- Set the terminal type
-config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Tokyo Night"
 -- For example, changing the color scheme:
 -- config.colors = require("cyberdream")
+config.color_scheme = "Shades of Purple (base16)"
 config.default_prog = { "pwsh.exe", "-NoLogo" }
+
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font("ComicShannsMono Nerd Font")
+
+config.font = wezterm.font("ComicShanns Nerd Font")
 -- config.font = wezterm.font("Iosevka Nerd Font")
 -- config.font = wezterm.font("Hack Nerd Font")
--- config.font = wezterm.font("Incoslanta Nerd Font", { weight = "Light", stretch = "Expanded" })
+-- config.font = wezterm.font("Inconsolata Nerd Font", { weight = "Regular", stretch = "Expanded" })
 -- config.font = wezterm.font("Fixedsys Excelsior")
-
-config.front_end = "OpenGL"
-config.max_fps = 144
-config.initial_cols = 80
 config.font_size = 16
-config.window_background_opacity = 0.75 -- Set window opacity to 95% for better readability
--- config.window_background_opacity = 0.7
--- config.win32_system_backdrop = "Acrylic"
 
--- config.foreground_text_hsb = {
--- 	hue = 1.0,
--- 	saturation = 1.2,
--- 	brightness = 1.5,
--- }
-
+-- config.window_background_opacity = 0.75 -- Set window opacity to 95% for better readability
+config.window_background_opacity = 0.75
+config.win32_system_backdrop = "Acrylic"
 -- config.window_background_image = bg_image
 -- config.window_background_image_hsb = {
 -- 	-- Darken the background image by reducing it to 1/3rd
@@ -57,10 +57,24 @@ config.window_background_opacity = 0.75 -- Set window opacity to 95% for better 
 -- 	hue = 1.0,
 --
 -- 	-- You can adjust the saturation also.
--- 	saturation = 0.8,
+-- 	saturation = 1.0,
 -- }
-
+-- -- config.foreground_text_hsb = {
+-- -- 	hue = 1.0,
+-- -- 	saturation = 1.2,
+-- -- 	brightness = 1.5,
+-- -- }
 config.keys = {
+	{
+		key = "b",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action_callback(function(window)
+			bg_image = pick_random_background(background_folder)
+			window:set_config_overrides({
+				window_background_image = bg_image,
+			})
+		end),
+	},
 	{
 		key = "G",
 		mods = "CTRL|SHIFT",
